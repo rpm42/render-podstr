f6bd0a5a7fc7c16a5b1588376c0d05a583dbff0a
rpm42 <rpm42@ya.ru>
2022-12-16 22:50:47 +0300

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import * as React from 'react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 
-import App from './App';
+import App from './App'
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
 
 root.render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
